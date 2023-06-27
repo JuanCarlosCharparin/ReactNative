de888624b69689bc935b462a61164ddcbc089124
Juan Carlos Charparin <jccharparin@gmail.com>
2023-06-27 20:15:05 -0300

    Core Components

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView } from 'react-native';
+import { styles } from './styles';
+import { useState } from 'react';
 
 export default function App() {
+  const [borderColor, setBorderColor] = useState('#C5C9E7')
+
+  const onHandlerFocus = () => {
+    setBorderColor('#424D9E')
+  }
+
+  const onHandlerBlur = () => {
+    setBorderColor('#C5C9E7')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Holaaa Juan Carlos</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={[styles.input, { borderColor: borderColor}]} 
+            placeholder='add new tasks'
+            autoCapitalize='none'
+            autoCorrect={false}
+            cursorColor='#424D9E'
+            selectionColor='#D4D7ED'
+            placeholderTextColor='#C5C9E7'
+            onFocus={onHandlerFocus}
+            onBlur={onHandlerBlur}
+          />
+          <Button title='Create' color='#424D9E' />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
